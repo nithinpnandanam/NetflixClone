b51b1fd92d0e5ab4cc45c24c24c9d470b4b87950
Nithin Prakash <nithinpnandanam@gmail.com>
2024-01-26 13:27:57 +0530

    set up react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import Body from "./Components/Body"
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Body/>
     </div>
   )
 }
