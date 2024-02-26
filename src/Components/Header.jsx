495746d1eb0ea0a729994b641dfaff08667d70fb
Nithin Prakash <nithinpnandanam@gmail.com>
2024-02-26 23:49:51 +0530

    Fixed browser redirection

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,19 +2,57 @@ import { signOut } from "firebase/auth";
 import { auth } from "../Utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import {onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import {addUser,removeUser} from "../Utils/userSlice"
+import { useEffect } from "react";
+
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
   const logOut = () => {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
-        navigate("/");
+
       })
       .catch((error) => {
         // An error happened.
       });
   };
+  // header is present in all pages
+  // When the header loads onAuthStateChanged will be called in the initial render 
+  // Since user is not logged in that case "Log Out in onAuthStateChanged" will br printed 
+  // When we log in using the firebase api the authentication state changes
+  // onAuthStateChanged(auth, (user) => { ... });: This function sets up a listener for authentication state changes.
+  // So when the state changes the call back function is triggered
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log("user",user)
+      if (user) {
+        console.log("Log in onAuthStateChanged")
+        // User is signed in, see docs for a list of available properties
+        const { uid, email, displayName,photoURL} = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL:photoURL
+          })
+        );
+        navigate("/browse");
+        // ...
+      } else {
+        // User is signed out
+        console.log("Log Out in onAuthStateChanged")
+        dispatch(removeUser());
+        navigate("/ ")
+      }
+    });
+  }, []);
 
   return (
     <div className="px-8 py-2 bg-gradient-to-b from-black flex justify-between items-center">
